2fca1346bcc49c1f6f8c278ad328fc7bcfb821a2
JAYSH <sanjaysheombar@gmail.com>
2025-01-05 13:14:36 +0100

    inject user repository in authentication service

--- src/main/kotlin/nl/jaysh/calories/features/authentication/AuthenticationService.kt
+++ src/main/kotlin/nl/jaysh/calories/features/authentication/AuthenticationService.kt
@@ -1,8 +1,9 @@
 package nl.jaysh.calories.features.authentication
 
+import nl.jaysh.calories.core.data.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
-class AuthenticationService {
+class AuthenticationService(private val repository: UserRepository) {
 
 }
